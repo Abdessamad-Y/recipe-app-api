643e06cdd9fbfd3e0bf6b062cddf315b4a57214f
Abdessamad-Y <abdessamad.yabes@gmail.com>
2023-04-23 13:53:14 +0100

    fixed linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,10 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     return x + y
 
-def substract(x,y):
-    return y-x
+
+def substract(x, y):
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,18 +4,19 @@ Sample tests
 
 from django.test import SimpleTestCase
 
-from app import calc 
+from app import calc
+
 
 class CalcTestCase(SimpleTestCase):
     """Test the calc module. """
 
     def test_add_numbers(self):
         """Testing add number agains"""
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_sunstract_numbers(self):
         """Testing sunstract numbers"""
-        res = calc.substract(10,15)
+        res = calc.substract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
