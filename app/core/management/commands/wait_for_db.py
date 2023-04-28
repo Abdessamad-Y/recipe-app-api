c764d2eb19ad6238b7e9ddc4784543c48220b1c1
abdessamad yabes <abdessamad.yabes@bso.co>
2023-04-28 10:48:27 +0100

    Write test for wait_db_command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command to wait for db is available 
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for db is available"""
+
+    def handle(self, *args, **options):
+        pass
