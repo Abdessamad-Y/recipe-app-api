8342870077f18422846766368f4946278af74350
Abdessamad-Y <abdessamad.yabes@gmail.com>
2023-05-20 15:48:58 +0100

    support creating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -35,6 +35,13 @@ class AdminSiteTests(TestCase):
     def test_edit_user_page(self):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
-        res = self.client.post(url)
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
